dc68d4e39d08210a1c2388b2ca327a64a7e00b7c
jogithschan <jogith2705@gmail.com>
2022-01-26 16:15:48 +0530

    Module 7 Complete

--- Module 7 - DOM/app.js
+++ Module 7 - DOM/app.js
@@ -1,18 +1,36 @@
-const h1 = document.getElementById('main-text');
+const ul = document.body.firstElementChild.nextElementSibling;
+console.log(ul);
+const firstLi = ul.firstElementChild;
+console.log(firstLi);
 
-h1.textContent = 'new title';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+const sect = document.querySelector('section');
+const button = document.querySelector('button');
+//sect.style.backgroundColor = 'blue';
+sect.className = '';
+button.addEventListener('click', () => {
+  // if(sect.className === 'red-bg visible'){
+  //   sect.className = 'red-bg invisible';
+  // } else {
+  //   sect.className = 'red-bg visible';
+  // }
+  sect.classList.toggle('invisible');
+  sect.classList.toggle('visible');
+})
+// const h1 = document.getElementById('main-text');
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + ' modified!';
+// h1.textContent = 'new title';
+// h1.style.color = 'white';
+// h1.style.backgroundColor = 'black';
 
-//special property to select body or head
-const body = document.body;
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + ' modified!';
 
-// const listItemElements = document.querySelectorAll('li');
-const listItemElements = document.getElementsByTagName('li');
+// //special property to select body or head
+// const body = document.body;
 
-for(const listItem of listItemElements){
-  console.dir(listItem);
-}
+// // const listItemElements = document.querySelectorAll('li');
+// const listItemElements = document.getElementsByTagName('li');
+
+// for(const listItem of listItemElements){
+//   console.dir(listItem);
+// }
